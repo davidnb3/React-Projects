3716fcde9afadf48330726321576448f8b30f026
kosmical <david.mario.pieri@gmail.com>
2021-04-02 16:50:55 +0200

    completed project 1. Added remove button

--- 1-birthday-reminder/setup/src/App.js
+++ 1-birthday-reminder/setup/src/App.js
@@ -1,8 +1,24 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
+
+
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data);
+
+  function handleRemove(id) {
+    const updatePeople = people.filter((person) => person.id !== id);
+    setPeople(updatePeople);
+  }
+  return <main>
+    <section className='container'>
+      <h3>{people.length} birthdays today</h3>
+      <List people={people} onRemove={handleRemove} />
+      <button className='btn' onClick={() => setPeople([])}>
+        Clear all
+      </button>
+    </section>
+  </main>
 }
 
 export default App;
